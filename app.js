52852e68cd3b79464d8b741305a1e541f677761d
ahmet kizilay <ahmet.kizilay@gmail.com>
2015-10-09 23:41:23 +0300

    added gc-map as embed, set up css

--- app.js
+++ app.js
@@ -6,11 +6,15 @@ var session = require("express-session");
 var logger = require("morgan");
 var bodyParser = require("body-parser");
 
+var http = require("http");
+var socketIO = require("socket.io");
+
 var passport = require("passport");
 var LinkedInStrategy = require("passport-linkedin-oauth2").Strategy;
 
 var saveProfile = require("./integrate.js");
 var db = require("./db.js");
+
 
 var LINKEDIN_API_KEY = process.env.LINKEDIN_API_KEY;
 var LINKEDIN_SECRET_KEY = process.env.LINKEDIN_SECRET_KEY;
@@ -38,9 +42,7 @@ passport.use(new LinkedInStrategy({
 function(req, accessToken, refreshToken, profile, done) {
   req.session.accessToken = accessToken;
   process.nextTick(function() {
-    console.log("before initial get")
     db.get(profile.id).then(function (obj) {
-      console.log("after get");
       var userObj = {
         id: profile.id,
         name: profile.displayName
@@ -57,20 +59,43 @@ function(req, accessToken, refreshToken, profile, done) {
 }));
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+var sessionMW = session({
+  secret: "123123h123jk123hk",
+  resave: false,
+  saveUninitialized: false
+});
+
+var clients = {};
+
+io.use(function (socket, next) {
+  sessionMW(socket.request, socket.request.res, next);
+});
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "jade");
 app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(session({
-  secret: "123123h123jk123hk",
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(sessionMW);
 app.use(passport.initialize());
 app.use(passport.session());
 
+io.on('connection', function(socket){
+  clients[socket.request.sessionID] = socket.id;
+
+  socket.on("start-save", function() {
+    saveProfile(socket.request.session.passport.user.id, socket.request.session.accessToken).then(function (memberData) {
+        return db.put(socket.request.session.passport.user.id);
+      }).then(function() {
+        socket.emit("login-completed");
+      });
+  });
+
+});
 
 app.get("/", function (req, res) {
   if (req.user) {
@@ -90,27 +115,45 @@ app.get("/login", function (req, res) {
   res.render("login");
 });
 
+app.get("/process", function (req, res) {
+  res.render("process");
+});
+
 app.get("/auth/login", passport.authenticate("linkedin", { state: "temp_state" }), function (req, res) {
   // wont be called
 });
 
 app.get("/auth/callback",
   passport.authenticate("linkedin", { failureRedirect: "/"}), function (req, res) {
-    console.log(req.user);
-    if (!req.user.saved) {
-
-      saveProfile(req.user.id, req.session.accessToken).then(function (memberData) {
-        return db.put(req.user.id);
-      }).then(function() {
-        console.log("this should be saved now");
-      });
-    }
+    // if (!req.user.saved) {
+    //
+    //   saveProfile(req.user.id, req.session.accessToken).then(function (memberData) {
+    //     return db.put(req.user.id);
+    //   }).then(function() {
+    //     console.log("this should be saved now");
+    //     console.log("session id is", req.sessionID);
+    //     console.log("the socket id is ", clients[req.sessionID]);
+    //     io.sockets.socket(clients[req.sessionID]).emit("login-completed");
+    //   });
+    // }
+    // else {
+    //   console.log("session id is", req.sessionID);
+    //   console.log("the socket id is ", clients[req.sessionID]);
+    //   io.sockets.socket(clients[req.sessionID]).emit("login-completed");
+    // }
     //saveProfile(req.user.id, req.session.accessToken):;
-    res.render("map");
+    //res.render("map");
+    if (req.user) {
+      res.redirect("/process");
+    }
+    else {
+      res.redirect("/login", {err: true});
+    }
   }
+
 );
 
-var server = app.listen(process.env.PORT || 3000, function() {
+server.listen(process.env.PORT || 3000, function() {
   var host = server.address().address;
   var port = server.address().port;
 
---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('sass', function () {
+  gulp.src('./scss/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./public/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
